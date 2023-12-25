46d24199b117d805cf958c4db1415e92f2fc5498
yuhi yamane <bird9.yuhi@gmail.com>
2023-12-25 16:23:34 +0900

    fix: added files

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,3 +1,4 @@
+use std::ffi::c_float;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -7,5 +8,10 @@ extern {
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    alert(&format!("Hello, {}!", name))
+}
+
+#[wasm_bindgen]
+pub fn calc(a: c_float, b: c_float) -> c_float {
+    return  a + b
 }
