ed3d52118fbd5809378f8ec9938736197855091a
yuhi yamane <bird9.yuhi@gmail.com>
2023-12-25 15:55:15 +0900

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@
 import React, { useState, useEffect } from 'react';
 import CalculationTab from './CalculationTab';
 import calculationsData from './assets/pages.json';
-import * as wasm from '../wasm/pkg/wasm';
+import init, {calc} from '../wasm/pkg/wasm';
 import { Calculation } from './types.ts';
 const App: React.FC = () => {
     const [calculations, setCalculations] = useState<Calculation[]>([]);
 
     useEffect(() => {
+        init()
         setCalculations(calculationsData.calculations);
     }, []);
 
     const performCalculation = (input1: number, input2: number): number =>  {
         // ここで wasm の関数を呼び出す
-        return wasm.calc(input1, input2)
+        return calc(input1, input2)
     };
 
     return (
